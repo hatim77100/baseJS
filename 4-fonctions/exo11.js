76fadb3e824c51270a4b991457eaccc3a98757b0
hatim77100 <najoua-hatim@hotmail.fr>
2022-09-19 23:15:31 +0200

    update exo11

--- 4-fonctions/exo11.js
+++ 4-fonctions/exo11.js
@@ -1,18 +1,35 @@
 var readline = require("readline-sync");
-// menu();
-// var choix = readline.questionInt("Quel est votre choix ? ");
-// var nombre = readline.questionInt("Quel nombre voulez-vous ? ");
-
-// function menu() {
-//     console.log("*****************");
-//     console.log("1/ Factorielle\n2/ Somme\n3/Quitter");
-//     console.log("*****************");
-// }
+menu();
+var choix = readline.questionInt("Quel est votre choix ? ");
+var nombre = readline.questionInt("Quel nombre voulez-vous ? ");
 let result = 1;
-function factorielle(factor) {
-    for(let i = 1; i <= factor; i++) {
-        result*=i
-    }
-    console.log(result);
+let resultat = 0;
+
+
+switch(choix) {
+    case 1 : function factorielle(nombre) {
+        for(let i = 1; i <= nombre; i++) {
+            result*=i
+        }
+        console.log("Le resultat de ! " + nombre + " est : " + result)
+    }; 
+    break;
+    case 2 :  
+    break;
+    case 3 : console.log("A+");
 }
-factorielle(5); 
+
+function menu() {
+    console.log("*****************");
+    console.log("1/ Factorielle\n2/ Somme\n3/ Quitter");
+    console.log("*****************");
+}
+
+
+factorielle(nombre); 
+function somme(nombre) {
+    for(let i = 0; i <= nombre; i++){
+        resultat= resultat + i; 
+    }
+    console.log("Le resultat de la somme des " + nombre + " premiers nombre est : "+ resultat);
+}
